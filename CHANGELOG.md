acb61b032a7603a15f67adb9108f4d6500cce235
Daniel Richter <danielrichter@posteo.de>
2019-12-06 16:47:07 +0100

    2.3.8

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+#3.2.8
+Initial Release of this chocolatey package
---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/imolorhe/altair/releases/download/v2.3.8/altair_2.3.8_win.exe'
+$url64      = 'https://github.com/imolorhe/altair/releases/download/v2.3.8/altair_2.3.8_win.exe'
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+  softwareName  = 'altair-graphql*'
+  checksum      = ''
+  checksumType  = 'sha256'
+  checksum64    = ''
+  checksumType64= 'sha256'
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
